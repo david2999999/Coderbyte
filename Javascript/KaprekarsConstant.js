9d6deed0051414ecec53e7bc72b1db6a7c457289
David Jiang <djiang86@binghamton.edu>
2018-02-15 22:18:16 +0000

    KaprekarsConstant.js

---
+++ Javascript/KaprekarsConstant.js
@@ -0,0 +1,33 @@
+// Using the JavaScript language, have the function KaprekarsConstant(num) take the num parameter being
+// passed which will be a 4-digit number with at least two distinct digits. Your program should perform the 
+// following routine on the number: Arrange the digits in descending order and in ascending order 
+// (adding zeroes to fit it to a 4-digit number), and subtract the smaller number from the bigger number. 
+// Then repeat the previous step. Performing this routine will always cause you to reach a fixed number: 6174.
+// Then performing the routine on 6174 will always give you 6174 (7641 - 1467 = 6174). Your program should return 
+// the number of times this routine must be performed until 6174 is reached. For example: if num is 3524 your program should return 3 because
+// of the following steps: (1) 5432 - 2345 = 3087, (2) 8730 - 0378 = 8352, (3) 8532 - 2358 = 6174. 
+
+
+function KaprekarsConstant(num) { 
+
+    var counter = 0, biggerNum, smallerNum;
+    
+  while(1){
+       smallerNum = parseInt(num.toString().split("").sort().join(""));
+        biggerNum = parseInt(num.toString().split("").sort(function(a,b){
+            return b - a;
+        }).join(""));
+        
+      num = biggerNum - smallerNum;
+      counter++;
+
+              
+      if(num === 6174)
+        return counter;
+  }
+
+   
+}
+   
+// keep this function call here 
+KaprekarsConstant(readline());  
